908d7e0145e8314bf404fce6855bb246fe9b6588
caro553 <57499404+caro553@users.noreply.github.com>
2020-05-31 15:30:05 +0200

    Add files via upload

---
+++ music.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Music Player</title>
+<script defer src="https://use.fontawesome.com/release/v5.0.3/js/all.js">
+</head>
+<body>
+
+<audio id="player">
+    <source id="source">
+</audio>
+
+<div id="controls">
+    <i class="far fa-play-circle fa-5x"></i>
+    <i class="far fa-pause-circle fa-5x"></i>
+    <p>Volume:</p>
+    <input type="text">
+</div>
+
+<h4>My Song:</h4>
+
